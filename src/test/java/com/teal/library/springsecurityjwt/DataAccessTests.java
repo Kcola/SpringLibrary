cb6e92265f3414f82b35c8216270f3b01490066a
Kolawole Campbell <kolawole.campbell@gmail.com>
2020-04-18 18:02:10 -0400

    stopping point

--- src/test/java/com/teal/library/springsecurityjwt/DataAccessTests.java
+++ src/test/java/com/teal/library/springsecurityjwt/DataAccessTests.java
@@ -1,22 +1,55 @@
 package com.teal.library.springsecurityjwt;
 
+import com.teal.library.springsecurityjwt.models.BorrowsEntity;
+import com.teal.library.springsecurityjwt.models.UsersEntity;
+import com.teal.library.springsecurityjwt.viewmodels.UserForm;
+import org.hibernate.Session;
+import org.hibernate.query.Query;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+import java.util.Random;
+
 import static org.junit.Assert.*;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DataAccessTests {
 	@Autowired
 	private DataAccess db;
-
 	@Test
 	public void ExistsUserTest() {assertTrue(db.ExistsUser("kola") == true);}
 	@Test
 	public void GetAllBooksTest() {
 		assertTrue(db.GetBooks().size() > 0);
 	}
-
+	@Test
+	public void GetUserInfoTest(){ assertTrue(db.GetUserInfo("bot").getFirstname().equals("Test")); }
+	@Test
+	public void DuplicateEmailTest(){
+		Session session = HibernateORM.getSessionFactory().openSession();
+		Query query = session.createQuery("from UsersEntity");
+		List<UsersEntity> list = query.list();
+		Random rand = new Random();
+		String email = list.get(rand.nextInt(list.size())).getEmail();
+		UsersEntity testUser = new UsersEntity();
+		testUser.setUsername("tester");
+		testUser.setFirstname("John");
+		testUser.setEmail(email);
+		testUser.setLastname("Doe");
+		testUser.setPassword("Blah");
+		testUser.setRole("user");
+		assertTrue(db.AddUser(testUser, 100)==-1);
+		UserForm testReader = new UserForm();
+		testReader.setEmail(email);
+		testReader.setAddress("");
+		testReader.setPassword("");
+		testReader.setZipcode("");
+		testReader.setFirstname("Test");
+		testReader.setLastname("User");
+		assertTrue(db.AddReader(testReader)==-1);
+	}
 }
